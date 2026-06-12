717e3f46702437bb2b928ef65451d04514d6a83b
Fuad <f.mardhatillah@gmail.com>
2026-06-12 11:33:08 +0700

    fix PWA fullscreen

--- vite.config.js
+++ vite.config.js
@@ -7,25 +7,33 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
       manifest: {
         name: 'VALO - Kendali Keuanganmu',
         short_name: 'VALO',
         description: 'Kendali penuh atas hidupmu',
-        theme_color: '#1D9E75',
+        theme_color: '#080F1C',
         background_color: '#080F1C',
         display: 'standalone',
+        display_override: ['standalone', 'fullscreen'],
         orientation: 'portrait',
         start_url: '/',
+        scope: '/',
         icons: [
           {
             src: '/icons/icon-192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: '/icons/icon-512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
